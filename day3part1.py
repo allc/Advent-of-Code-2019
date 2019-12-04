968bd23e49c979835f6adccdad65c8c87a8d3eaf
Jinxuan Cui <i@cjxol.com>
2019-12-04 22:32:21 +0000

    Add my day 3 solutions

---
+++ day3part1.py
@@ -0,0 +1,60 @@
+'''
+https://adventofcode.com/2019/day/3
+
+Usage: cat day3part1input.txt | python3 day3part1.py
+'''
+
+
+wire1r = list(input().split(','))
+wire2r = list(input().split(','))
+
+
+wire1 = set()
+p = (0, 0)
+for w in wire1r:
+    if w[0] == 'R':
+        for _ in range(int(w[1:])):
+            p = (p[0] + 1, p[1])
+            wire1.add(p)
+    elif w[0] == 'D':
+        for _ in range(int(w[1:])):
+            p = (p[0], p[1] - 1)
+            wire1.add(p)
+    elif w[0] == 'L':
+        for _ in range(int(w[1:])):
+            p = (p[0] - 1, p[1])
+            wire1.add(p)
+    elif w[0] == 'U':
+        for _ in range(int(w[1:])):
+            p = (p[0], p[1] + 1)
+            wire1.add(p)
+
+
+wire2 = set()
+p = (0, 0)
+for w in wire2r:
+    if w[0] == 'R':
+        for _ in range(int(w[1:])):
+            p = (p[0] + 1, p[1])
+            wire2.add(p)
+    elif w[0] == 'D':
+        for _ in range(int(w[1:])):
+            p = (p[0], p[1] - 1)
+            wire2.add(p)
+    elif w[0] == 'L':
+        for _ in range(int(w[1:])):
+            p = (p[0] - 1, p[1])
+            wire2.add(p)
+    elif w[0] == 'U':
+        for _ in range(int(w[1:])):
+            p = (p[0], p[1] + 1)
+            wire2.add(p)
+
+
+intersects = wire1 & wire2
+
+
+min_dist = float('inf')
+for intersect in intersects:
+    min_dist = min(min_dist, abs(intersect[0]) + abs(intersect[1]))
+print(min_dist)
---
+++ day3part2.py
@@ -0,0 +1,78 @@
+'''
+https://adventofcode.com/2019/day/3
+
+Usage: cat day3part2input.txt | python3 day3part2.py
+'''
+
+
+wire1r = list(input().split(','))
+wire2r = list(input().split(','))
+
+
+wire1 = dict()
+p = (0, 0)
+d = 0
+for w in wire1r:
+    if w[0] == 'R':
+        for _ in range(int(w[1:])):
+            p = (p[0] + 1, p[1])
+            d += 1
+            if p not in wire1:
+                wire1[p] = d
+    elif w[0] == 'D':
+        for _ in range(int(w[1:])):
+            p = (p[0], p[1] - 1)
+            d += 1
+            if p not in wire1:
+                wire1[p] = d
+    elif w[0] == 'L':
+        for _ in range(int(w[1:])):
+            p = (p[0] - 1, p[1])
+            d += 1
+            if p not in wire1:
+                wire1[p] = d
+    elif w[0] == 'U':
+        for _ in range(int(w[1:])):
+            p = (p[0], p[1] + 1)
+            d += 1
+            if p not in wire1:
+                wire1[p] = d
+
+
+wire2 = dict()
+p = (0, 0)
+d = 0
+for w in wire2r:
+    if w[0] == 'R':
+        for _ in range(int(w[1:])):
+            p = (p[0] + 1, p[1])
+            d += 1
+            if p not in wire2:
+                wire2[p] = d
+    elif w[0] == 'D':
+        for _ in range(int(w[1:])):
+            p = (p[0], p[1] - 1)
+            d += 1
+            if p not in wire2:
+                wire2[p] = d
+    elif w[0] == 'L':
+        for _ in range(int(w[1:])):
+            p = (p[0] - 1, p[1])
+            d += 1
+            if p not in wire2:
+                wire2[p] = d
+    elif w[0] == 'U':
+        for _ in range(int(w[1:])):
+            p = (p[0], p[1] + 1)
+            d += 1
+            if p not in wire2:
+                wire2[p] = d
+
+
+intersects = wire1.keys() & wire2.keys()
+
+
+min_dist = float('inf')
+for intersect in intersects:
+    min_dist = min(min_dist, wire1[intersect] + wire2[intersect])
+print(min_dist)
